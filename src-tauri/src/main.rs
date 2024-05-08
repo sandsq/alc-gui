256e580cfcaf314a1acb96e5c80e0298bef5ce87
sandsq <116196797+sandsq@users.noreply.github.com>
2024-05-08 09:32:19 -0400

    pass config structs to backend when saving toml

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,7 @@
 use std::{fs, path::PathBuf};
 
 use strum::IntoEnumIterator;
-use alc::{alc_error::AlcError, keyboard::{key::PhalanxKey, layer::Layer, layout::Layout, layout_presets::{get_all_layout_size_presets, get_size_variant}}, optimizer::{config::{DatasetOptions, GeneticOptions, LayoutInfoTomlAdapter, LayoutOptimizerTomlAdapter, ScoreOptions}, optimize_from_toml, LayoutOptimizer}, text_processor::keycode::{generate_default_keycode_set, Keycode, KeycodeOptions}};
+use alc::{alc_error::AlcError, keyboard::{key::PhalanxKey, layer::Layer, layout::Layout, layout_presets::{get_all_layout_size_presets, get_size_variant}}, optimizer::{config::{DatasetOptions, GeneticOptions, LayoutInfoTomlAdapter, LayoutOptimizerConfig, LayoutOptimizerTomlAdapter, ScoreOptions}, optimize_from_toml, LayoutOptimizer}, text_processor::keycode::{generate_default_keycode_set, Keycode, KeycodeOptions}};
 use alc::keyboard::layout_presets::LayoutSizePresets::*;
 use tauri::{api::path::config_dir, Manager};
 
@@ -126,34 +126,21 @@ fn create_blank_layers(r: usize, c: usize, loc: String) -> Result<(String, Strin
 }
 
 #[tauri::command]
-fn write_toml(filename: &str, layout_info: LayoutInfoTomlAdapter, num_threads: usize, genetic_options: String, keycode_options: String, dataset_options: DatasetOptions, score_options: ScoreOptions) -> Result<(), AlcError> {
-	println!("writing {} with {:?}, {:?}", filename, layout_info, genetic_options);
-	let toml = format!("\
-	[layout_info]\n\
-	num_rows = {}\n\
-	num_cols = {}\n\
-	layout = \"\"\"\n\
-	{}\n\
-	\"\"\"\n\
-	effort_layer = \"\"\"\n\
-	{}\n\
-	\"\"\"\n\
-	phalanx_layer = \"\"\"\n\
-	{}\n\
-	\"\"\"\n\
-	[layout_optimizer_config]\n\
-	valid_keycodes = []\n\
-	num_threads = {}\n\
-	[layout_optimizer_config.genetic_options]\n\
-	{}\n\
-	[layout_optimizer_config.keycode_options]\n\
-	{}\n\
-	[layout_optimizer_config.dataset_options]\n\
-	{:?}\n\
-	[layout_optimizer_config.score_options]\n\
-	{:?}\n\
-	", layout_info.num_rows, layout_info.num_cols, layout_info.layout, layout_info.effort_layer, layout_info.phalanx_layer, num_threads,  genetic_options, keycode_options, dataset_options, score_options);
-	fs::write(filename, toml).unwrap_or_else(|_| panic!("unable to write file {}", filename));
+fn write_toml(filename: &str, layout_info: LayoutInfoTomlAdapter, num_threads: usize, genetic_options: GeneticOptions, keycode_options: KeycodeOptions, dataset_options: DatasetOptions, score_options: ScoreOptions) -> Result<(), AlcError> {
+	println!("writing {}", filename);
+	let layout_optimizer_config = LayoutOptimizerConfig {
+		genetic_options,
+		keycode_options,
+		valid_keycodes: vec![],
+		dataset_options,
+		score_options,
+		num_threads,
+	};
+	let adapter = LayoutOptimizerTomlAdapter {
+		layout_info,
+		layout_optimizer_config,
+	};
+	adapter.write_to_file(filename)?;
 	Ok(())
 }
 
